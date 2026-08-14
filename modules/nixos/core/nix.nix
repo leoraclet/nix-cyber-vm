daf69bb661040bd7434c4ff773436f068725a9cf
N£utr0nys <110538448+leoraclet@users.noreply.github.com>
2026-08-14 15:12:18 +0200

    Update nix.nix

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -1,13 +1,24 @@
 {
   flake.modules.nixos.nix = { config, ... }: {
     nix.enable = true;
-    nix.nixPath = [ "nixpkgs=${builtins.storePath <nixpkgs>}" ];
     nix.settings = {
       extra-experimental-features = [
         "nix-command"
         "flakes"
       ];
-      trusted-users = [ config.myConfig.userName ];
+      trusted-users = [
+        "root"
+        "wheel"
+
+        config.myConfig.userName
+      ];
+    };
+
+    nixpkgs.hostPlatform = "x86_64-linux";
+    system.stateVersion = config.myConfig.stateVersion;
+
+    nixpkgs.config = {
+      allowUnfree = true;
     };
   };
 }

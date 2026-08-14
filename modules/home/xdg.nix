656e2dc090f1162661964bedee7868a2a8243dc9
N£utr0nys <110538448+leoraclet@users.noreply.github.com>
2026-08-14 19:46:20 +0200

    Update xdg.nix

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -2,9 +2,6 @@
   flake.modules.homeManager.xdg =
     { ... }:
     {
-      # https://github.com/search?q=repo%3Anix-community%2Fhome-manager%20%20preferXdgDirectories&type=code
-      home.preferXdgDirectories = true;
-
       xdg = {
         enable = true;
 

675e8e5f430dce6e021cec0eef277d75b3f8a6b3
N£utr0nys <110538448+leoraclet@users.noreply.github.com>
2026-08-21 16:39:49 +0200

    Added extensions to brave

--- modules/home/browser/brave.nix
+++ modules/home/browser/brave.nix
@@ -4,9 +4,16 @@
       enable = true;
       package = pkgs.brave;
       extensions = [
-        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # Example: uBlock Origin
+        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # Ublock Origin
+        { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
+        { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark reader
+        { id = "oldceeleldhonbafppcapldpdifcinji"; } # Language Tool
+        { id = "chphlpgkkbolifaimnlloiipkdnihall"; } # OneTab
+        { id = "mdjildafknihdffpkfmmpnpoiajfjnjd"; } # Consent-O-Matic
       ];
-      commandLineArgs = [ "--disable-features=WebRtcAllowInputVolumeAdjustment" ];
+      commandLineArgs = [
+        "--disable-features=WebRtcAllowInputVolumeAdjustment"
+      ];
     };
   };
 }
